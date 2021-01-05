ac0e9fd8baa4a83ecf5b54f7d83f42b83f546e0d
nayabsadik <sadiksam07@gmail.com>
2021-01-05 15:00:25 +0530

    maven java project

---
+++ src/main/java/ContentsFile.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ContentsFile 
+{
+	public static void main(String[] args) 
+	{
+		try {
+			FileInputStream fin =new FileInputStream("E:\\textfile/sample.txt");
+			Scanner scan = new Scanner(fin);
+			while (scan.hasNextLine()) 
+			{
+				System.out.println(scan.nextLine());
+				
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+	}
+
+}
